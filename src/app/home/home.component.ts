361657b8c56c2b1049ce55cb38d3f06543976793
shiva ayyala <avnrsaishiva@gmail.com>
2023-08-31 11:56:57 +0530

    Login functionality

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -9,12 +9,15 @@ export class HomeComponent implements OnInit{
 
   carArray: any = [];
   filteredCarWidgets: any[] = [];
+  uniqueBrandNames: any = [];
   sortProperty: string = 'carName'; // Property to sort by
   sortOrder: 'asc' | 'desc' = 'asc'; // Sort order (asc or desc)
   
   ngOnInit() {
-    debugger;
-    this.carArray //=JSON.parse(JSON.stringify(carsData));;
+    this.loadCarData();
+  }
+  
+  loadCarData() {
     var request = new XMLHttpRequest();
     request.open(
       'GET',
@@ -25,19 +28,35 @@ export class HomeComponent implements OnInit{
     this.carArray = JSON.parse(request.responseText);
   }
 
+  extractUniqueBrandNames() {
+    const brandSet: any = [];
+    brandSet.add("All Brands"); 
+    brandSet.add(new Set(this.carArray.map((car:any) => car.brand)));
+    this.uniqueBrandNames = Array.from(brandSet);
+  }
+
   filterByBrand(event: Event) {
-    // const brandName = (event.target as HTMLSelectElement).value;
-    // if (brandName === "") {
-    //   this.filteredCarWidgets = [...this.loopArray]; // Reset to all items
-    // } else {
-    //   this.filteredCarWidgets = this.loopArray.filter(item => item.brandName === brandName);
-    // }
+    const brandName = (event.target as HTMLSelectElement).value;
+    console.log(brandName);
+    if (brandName === "") {
+      this.filteredCarWidgets = [...this.carArray]; // Reset to all items
+    } else {
+      this.filteredCarWidgets = this.carArray.filter((item: any) => {
+        return item.brand === brandName; // Adjust this line based on your data structure
+      });
+    }
   }
-
   sortCarWidgets() {
-    // this.loopArray.sort((a, b) => {
-    //   return this.sortOrder === 'asc' ? a.manufactureYear - b.manufactureYear : b.manufactureYear - a.manufactureYear;
-    // });
+    console.log("Before sorting:", this.carArray.map((car: any) => car.year));
+    this.carArray.sort((a: any, b: any) => {
+      const yearA = parseInt(a.year || "0", 10); // Convert to integer
+      const yearB = parseInt(b.year || "0", 10); // Convert to integer
+      return this.sortOrder === 'asc'
+        ? yearA - yearB
+        : yearB - yearA;
+    });
+    console.log("After sorting:", this.carArray);
   }
-
+  
+  
 }
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,18 +1,32 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router'; 
+import usersData from '../../assets/login.json';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css'] // You can add your styles here
+  styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+
   username: string = '';
   password: string = '';
+  users: any[] = usersData;
 
   constructor(private router: Router) {}
+  
 
   login() {
-    this.router.navigate(['/homePage']);
-  } 
+    const foundUser = this.users.find(user => user.userName === this.username && user.password === this.password);
+    if (foundUser) {
+      console.log('Login successful');
+      this.router.navigate(['/homePage']);
+      // Redirect or perform further actions for successful login
+    } else {
+      alert('Invalid username or password');
+      // Display an error message or handle unsuccessful login
+    }
+    
+
+  }
 }
